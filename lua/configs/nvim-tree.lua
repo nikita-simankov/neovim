9fc028f38ed557f6866d54d8569ce567d505cf44
Nikita Simankov <nikita.simankov.id@gmail.com>
2023-12-29 11:20:53 +0300

    Update nvim-tree config

--- lua/configs/nvim-tree.lua
+++ lua/configs/nvim-tree.lua
@@ -1,6 +1,9 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+  hijack_netrw = true,
+  disable_netrw = true,
+})
 
 vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>")
